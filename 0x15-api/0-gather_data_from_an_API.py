5c4c9912fbaa66fbeef964820b396f7eed024b53
Tamene Gelaye <tamenes21@gmail.com>
2023-01-16 14:27:07 +0300

    edited

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,24 +1,21 @@
 #!/usr/bin/python3
-"""
-Uses the JSON placeholder api to query data about an employee
-"""
+"""Getting data from API placeholder."""
 
-from requests import get
-from sys import argv
+import requests
+import sys
 
 if __name__ == '__main__':
-    main_url = 'https://jsonplaceholder.typicode.com'
-    todo_url = main_url + "/user/{}/todos".format(argv[1])
-    name_url = main_url + "/users/{}".format(argv[1])
-    todo_result = get(todo_url).json()
-    name_result = get(name_url).json()
+    """Gets API endpoint, then identify a user to display completed task info"""
+    endpoint = "https://jsonplaceholder.typicode.com/"
+    userId = sys.argv[1]
+    user = requests.get(endpoint + 'users/{}'.format(userId)).json()
+    todo = requests.get(endpoint + 'todos?userId={}'.format(userId)).json()
+    completed = []
 
-    todo_num = len(todo_result)
-    todo_complete = len([todo for todo in todo_result
-                         if todo.get("completed")])
-    name = name_result.get("name")
-    print("Employee {} is done with tasks({}/{}):"
-          .format(name, todo_complete, todo_num))
-    for todo in todo_result:
-        if (todo.get("completed")):
-            print("\t {}".format(todo.get("title")))
+    for task in todo:
+        if task.get("completed"):
+            completed.append(task.get("title"))
+    print("Employee {} is done with task({}/{}):"
+          .format(user.get('name'), len(completed), len(todo)))
+    for task in completed:
+        print('\t', task)
