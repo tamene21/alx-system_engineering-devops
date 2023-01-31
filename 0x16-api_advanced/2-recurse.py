0b5b6101473d4c69102bce6d5a110d0377e20b94
Tamene Gelaye <tamenes21@gmail.com>
2023-01-31 13:29:04 +0000

    title of hote post

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Query Reddit API recursively for all hot articles of a given subreddit
+"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], after="tmp"):
+    """
+        return all hot articles for a given subreddit
+        return None if invalid subreddit given
+    """
+    headers = requests.utils.default_headers()
+    headers.update({'User-Agent': 'Tamene Gelaye',
+                     'From': 'tgelaye21@gmail.com'})
+
+    # update url each recursive call with param "after"
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    if after != "tmp":
+        url = url + "?after={}".format(after)
+    r = requests.get(url, headers=headers, allow_redirects=False)
+
+    # append top titles to hot_list
+    results = r.json().get('data', {}).get('children', [])
+    if not results:
+        return hot_list
+    for e in results:
+        hot_list.append(e.get('data').get('title'))
+
+    # get next param "after" else nothing else to recurse
+    after = r.json().get('data').get('after')
+    if not after:
+        return hot_list
+    return (recurse(subreddit, hot_list, after))
