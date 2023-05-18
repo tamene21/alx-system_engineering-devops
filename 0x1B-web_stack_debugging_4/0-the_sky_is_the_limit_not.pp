799eee026496ea39172c1a010c8a112a85ea84b3
Tamene Gelaye <tamenes21@gmail.com>
2023-05-18 17:41:29 +0000

    Indentation corrected

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,12 +2,12 @@
 
 exec {'replace':
   provider => shell,
-  command => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/"  /etc/default/nginx',
+  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/"  /etc/default/nginx',
   before   => exec['restart'],
 }
 
 #Restart nginx
 exec {'restart':
   provider => shell,
-  command => 'sudo service nginx restart',
+  command  => 'sudo service nginx restart',
 }
