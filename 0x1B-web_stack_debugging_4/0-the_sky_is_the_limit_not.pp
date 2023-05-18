1de57d8279d6f09d87cd07c7aa3016e28d28a163
Tamene Gelaye <tamenes21@gmail.com>
2023-05-18 17:17:43 +0000

    coomand chang

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,13 @@
 #Increasing trafic twards nginx
+
 exec {'fix--for-nginx':
-  commdand => 'seed -i "s/15/4096" /etc/default/nginx',
+  provider => shell,
+  commdand => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/"  /etc/default/nginx',
   path     => '/usr/bin/env/'
 }
 
 #Restart nginx
 exec {'restart-nginx':
-  command => 'nginx restart',
-  path    => '/etc/init.d' 
+  provider => shell,
+  command => 'sudo nginx restart',
 }
